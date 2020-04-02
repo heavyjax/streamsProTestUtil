17c403f177bacdbf2927b4055f1c37b411a43356
heavyjax <sa.maxim1@yandex.ru>
2020-04-02 15:36:39 +0300

    Initial commit

---
+++ src/main/java/ru/sbrf/Main.java
@@ -0,0 +1,9 @@
+package ru.sbrf;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("========================");
+        System.out.println("Hello");
+        System.out.println("========================");
+    }
+}
